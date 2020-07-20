d2758766b5ce669b901aab49e892fbca4b170830
jno <jno@pisem.net>
2020-07-20 11:52:17 +0300

    rename the package; add comments, etc

--- bytes.go
+++ bytes.go
@@ -1,27 +1,46 @@
-package types
+package utypes
 
 import "strconv"
 
 // Simple counter-like type with fancy .String() formatter.
+// The value becomes formatted as for "%7.3f" with binary SI suffix followed by `B`.
+// Suffixes are from `Ki` (kilo-, 2^10) to `Ei` (exa-, 2^60).
+// .
+// It is supposed to declare a
+// 	var traffic Bytes
+// and use `traffic.Add(size)` to accumulate the value, then
+// use (implicitely) `traffic.String()` to make pretty print.
+// .
 // One may find useful .Rate(secs) or .BitRate(secs) formatters too.
+// .
+// The formatted Bytes value takes up to 10 character positions.
+// The formatted Rate() value takes up to 12 c.p.
+// The formatted BitRate() value takes up to 13 c.p.
+// .
+// NOTE: They are "traffic bytes"! The "disk bytes" are usually decimal, not binary.
 type Bytes uint64
 
 const (
-	BITS_PER_BYTE = 8
-	JUSTBYTES     = "B"
-	KILOBYTE      = Bytes(1) << 10
-	KILOBYTES     = "KiB"
-	MEGABYTE      = Bytes(1) << 20
-	MEGABYTES     = "MiB"
-	GIGABYTE      = Bytes(1) << 30
-	GIGABYTES     = "GiB"
-	TERABYTE      = Bytes(1) << 40
-	TERABYTES     = "TiB"
-	PETABYTE      = Bytes(1) << 50
-	PETABYTES     = "PiB"
-	EXABYTE       = Bytes(1) << 60
-	EXABYTES      = "EiB"
-	BYTES_MAX     = Bytes(0xffffffffffffffff)
+	BITS_PER_BYTE        = 8
+	JUSTBYTES            = "B"
+	KILOBYTE             = Bytes(1) << 10
+	KILOBYTES            = "KiB"
+	MEGABYTE             = Bytes(1) << 20
+	MEGABYTES            = "MiB"
+	GIGABYTE             = Bytes(1) << 30
+	GIGABYTES            = "GiB"
+	TERABYTE             = Bytes(1) << 40
+	TERABYTES            = "TiB"
+	PETABYTE             = Bytes(1) << 50
+	PETABYTES            = "PiB"
+	EXABYTE              = Bytes(1) << 60
+	EXABYTES             = "EiB"
+	BYTES_MAX            = Bytes(0xffffffffffffffff)
+	BYTES_VALUE_BITSIZE  = 64
+	BYTES_VALUE_FORMAT   = 'f'
+	BYTES_VALUE_DECIMALS = 3
+	BYTES_PER_SECOND     = "/s"
+	BITS_PER_SECONDS     = " bps"
 )
 
 func (v *Bytes) Add(n uint64) *Bytes {
@@ -40,8 +59,15 @@ func (v *Bytes) IncrementBy(n Bytes) *Bytes { return v.Add(n.Uint()) }
 func (v *Bytes) Assign(n Bytes) *Bytes      { return v.Set(n.Uint()) }
 
 func (v Bytes) format(scale Bytes) string {
-	return strconv.FormatFloat(float64(v)/float64(scale), 'f', 3, 64)
+	return strconv.FormatFloat(
+		float64(v)/float64(scale),
+		BYTES_VALUE_FORMAT,
+		BYTES_VALUE_DECIMALS,
+		BYTES_VALUE_BITSIZE,
+	)
 }
+
+// format byte count value with binary SI suffixes
 func (v Bytes) String() string {
 	if v >= EXABYTE {
 		return v.format(EXABYTE) + EXABYTES
@@ -64,8 +90,9 @@ func (v Bytes) String() string {
 	return strconv.FormatUint(uint64(v), 10) + JUSTBYTES
 }
 
+// format bytes-per-second value with binary SI suffixes (KiB to EiB)
 func (v Bytes) Rate(seconds float64) string {
-	return Bytes(float64(v)/seconds).String() + "/s"
+	return Bytes(float64(v)/seconds).String() + BYTES_PER_SECOND
 }
 
 // calculate bits-per-second value
@@ -80,9 +107,11 @@ func (v Bytes) Bps(seconds float64) (r float64) {
 	}
 	return
 }
+
+// format bits-per-second value with binary SI suffixes
 func (v Bytes) BitRate(seconds float64) string {
 	r := Bytes(v.Bps(seconds)).String()
-	return r[:len(r)-1] + " bps"
+	return r[:len(r)-1] + BITS_PER_SECONDS
 }
 
 // vim:set ai:EOF //
--- bytes_test.go
+++ bytes_test.go
@@ -1,4 +1,4 @@
-package types
+package utypes
 
 import "fmt"
 
